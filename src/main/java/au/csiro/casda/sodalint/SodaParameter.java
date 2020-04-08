5de7a84a5a6b073771f412901451ae4e91f5fc36
tra155 <Christopher.Trapani@csiro.au>
2020-04-08 16:20:28 +1000

    Update SODA lint.  Band parameter UCD updated to 'em.wl;stat.interval'

--- src/main/java/au/csiro/casda/sodalint/SodaParameter.java
+++ src/main/java/au/csiro/casda/sodalint/SodaParameter.java
@@ -29,7 +29,7 @@ public enum SodaParameter
     /** The general POS filter parameter for spatial regions. */
     POS("phys.angArea;obs", "", "char", "*", "region"), 
     /** The BAND filter parameter for wavelength cutouts. */
-    BAND("em.wl", "m", "double", "2", "interval"), 
+    BAND("em.wl;stat.interval", "m", "double", "2", "interval"), 
     /** The TIME filter parameter for time cutouts. */
     TIME("time.interval;obs.exposure", "d", "double", "2", "interval"), 
     /** The POL filter parameter for polarisation cutouts. */
